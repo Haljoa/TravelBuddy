f731c50464ca6492ca15b4ce1f50381b87bd8cb7
alexan0301 <alexan0301@gmail.com>
2025-10-01 11:28:44 +0200

    Fra og til imput

---
+++ API/src/main/java/FraogTilInput.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class FraogTilInput {
+    private int fra;
+    private int til;
+
+    public void setFra(int fra) {
+        this.fra = fra;
+    }
+
+    public int getFra() {
+        return fra;
+    }
+
+    public void setTil(int til) {
+        this.til = til;
+    }
+
+    public int getTil() {
+        return til;
+    }
+
+    public static int Frastopp(int fra)  {
+        Scanner Frastopp = new Scanner(System.in);
+        System.out.println("Start");
+        fra = Frastopp.nextInt();
+        return fra;
+    }
+    public static int Tilstopp(int til)  {
+
+        Scanner Tilstopp = new Scanner(System.in);
+        System.out.println("Slutt stopp");
+        til = Tilstopp.nextInt();
+        return til;
+    }
+
+}
