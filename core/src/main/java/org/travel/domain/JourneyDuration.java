398f207415d52a957c4c1090d791957a53df48fe
NightTapes <ole98tvedt@gmail.com>
2025-10-05 06:00:53 +0200

    kødda, glemte å stage. nå -> laget kjerne delen av koden for hvor lang tid reisen tok

--- core/src/main/java/org/travel/domain/JourneyDuration.java
+++ core/src/main/java/org/travel/domain/JourneyDuration.java
@@ -1,4 +1,25 @@
 package org.travel.domain;
 
 public class JourneyDuration {
+
+    private int from;
+    private int to;
+    private int durationInMinutes;
+
+    public JourneyDuration(int from, int to, int durationInMinutes) {
+        this.from = from;
+        this.to = to;
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public int getFrom() {return from;}
+    public int getTo() {return to;}
+    public int getDurationInMinutes() {return durationInMinutes;}
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        if (durationInMinutes <= 0) {
+            throw new IllegalArgumentException("Venligst skriv inn et gyldig tall.");
+        }
+        this.durationInMinutes = durationInMinutes;
+    }
 }
