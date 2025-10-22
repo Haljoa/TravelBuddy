b16852e5826dcdd1475e084e936ef50e2fa252bb
NightTapes <ole98tvedt@gmail.com>
2025-10-23 00:35:09 +0200

    TripDataService må sees på fortsatt tror jeg :)

--- src/main/java/org/travel/Adapters/TripDataMongoAdapter.java
+++ src/main/java/org/travel/Adapters/TripDataMongoAdapter.java
@@ -1,10 +1,18 @@
 package main.java.org.travel.Adapters;
 
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.ReplaceOptions;
+import main.java.org.travel.core.Ports.TripDataRepository;
 import main.java.org.travel.core.domain.TripData;
+import org.bson.conversions.Bson;
 
-public class TripDataMongoAdapter {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripDataMongoAdapter implements TripDataRepository {
     //denne klassen/adapteren står for kommunikasjonen mellom core og databasen vår.
     //den lagrer, oppdaterer, og leser TripData objekter fra og til databasen.
 
@@ -18,4 +26,44 @@ public class TripDataMongoAdapter {
     }
 
     //CRUD operasjoner under her
+    //alt dette er funnet på MongoDB Java Driver sine offisielle docs sider under CRUD Operations:
+    //https://www.mongodb.com/docs/drivers/java/sync/current/
+    @Override
+    public void saveTripData(TripData tripData) {
+        //bruker et filter til å finne samme routeId i databasen som vi sender med som et TripData objekt
+        Bson idMatch = Filters.eq("sett inn riktig primær nøkkel i fra DB her", tripData.getRouteId());
+        //bytter ut gammel info om ruten ved å overskrive den med ny data, eller lagre ny data hvis ingen finnes
+        try {
+            collection.replaceOne(idMatch, tripData, new ReplaceOptions().upsert(true));
+            System.out.println("Route has been updated.");
+        } catch (MongoException exception) {//MongoException tar hånd om alle feil relatert til databasen
+            System.err.println("Something went wrong. " + exception.getMessage());
+        }
+    }
+
+    @Override
+    public TripData findRouteById(String routeId) {
+        try {
+            //finner det første dokumentet i DB hvor PK er lik ruteIDen vi sender med.
+            TripData route = collection.find(Filters.eq("sett inn riktig primær nøkkel i fra DB her", routeId)).first();
+            return route;
+        } catch (MongoException exception) {
+            System.err.println("Something went wrong. " + exception.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public List<TripData> findAllTrips() {
+        //finner alle dokumenter i database collectionen som er definert i konstruktøren i denne klassen,
+        //og legger dem inn i en liste.
+        List<TripData> allTrips = new ArrayList<>();
+        try {
+            collection.find().into(allTrips);
+            System.out.println("Fetched " + allTrips.size() + " trips.");
+        } catch (MongoException exception) {
+            System.err.println("Something went wrong. " + exception.getMessage());
+        }
+        return allTrips;
+    }
 }
--- src/main/java/org/travel/core/Ports/TripDataRepository.java
+++ src/main/java/org/travel/core/Ports/TripDataRepository.java
@@ -1,4 +1,21 @@
 package main.java.org.travel.core.Ports;
 
+import main.java.org.travel.core.domain.TripData;
+
+import java.util.List;
+
 public interface TripDataRepository {
+    //slik jeg forstod det, kan porter sees på som stikkontakter på utsiden av kjernen i arkitekturen.
+    //stikkontakten bestemmer hvordan noe som plugges inn i den må se ut, men ikke nødvendigvis hvordan
+    //det må fungere eller oppføre seg.
+    //denne porten definerer da hvordan adapteren TripDataMongoAdapter, som "kobles til" her for å skape en kobling
+    //mellom databasen vår og kjernen, må se ut for at tilkoblingen skal fungere.
+    //dette blir CRUD operasjoner til og fra databasen.
+
+    //for lagring av ekstra data om ruten
+    void saveTripData(TripData tripData);
+    //for å hente data om én rute basert på Iden dens
+    TripData findRouteById(String routeId);
+    //henting av all data om alle ruter
+    List<TripData> findAllTrips();
 }
