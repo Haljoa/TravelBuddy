11d6ef015efa9919c131d9459278b99a032d029c
alexan0301 <alexan0301@gmail.com>
2025-11-11 13:55:02 +0100

    kode for output fra entur

--- src/main/java/org/travel/Core/Main.java
+++ src/main/java/org/travel/Core/Main.java
@@ -1,7 +1,23 @@
 package org.travel.Core;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.travel.API.GraphQLClient;
+import org.travel.Core.Domain.Journeymaker;
+
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+
+public static void main(String[] args) throws Exception {
+    Journeymaker jm = new Journeymaker(GraphQLClient.JourneyV3Query("NSR:StopPlace:2534", "NSR:StopPlace:60053"));
+    List<JsonNode> patterns = jm.splitTripSegments();
+
+    System.out.println("Trip Patterns:");
+    for (JsonNode pattern : patterns) {
+        // Pretty-print each JSON object
+        System.out.println(pattern.toPrettyString());
 
     }
 }
+    }
+
