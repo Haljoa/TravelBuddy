264665e9095198c107caea5e1902e28c749c129f
Haljoa <Haldoroie@gmail.com>
2025-11-14 12:29:57 +0100

    La til noen andre smådetaljer

--- src/main/org/travel/Core/Domain/Users/Bruker.java
+++ src/main/org/travel/Core/Domain/Users/Bruker.java
@@ -1,5 +1,8 @@
 package org.travel.Core.Domain.Users;
 
+//Overklassen til alle de forskjellige brukertypene vi kan lage. Definerer universell funksjonalitet for alle klassene
+//De forskjellige brukertypene ville hatt sine egne funksjoner, hvis vi hadde tid til å implementere det ordentlig
+
 public abstract class Bruker {
 
     private final int AccessLevel = 0;
--- src/main/org/travel/Core/Service/UserCreater.java
+++ src/main/org/travel/Core/Service/UserCreater.java
@@ -3,4 +3,10 @@ package org.travel.Core.Service;
 import org.travel.Core.Domain.Users.*;
 
 public class UserCreater {
+
+    //Denne klassen er her for å vise at vi har tenkt på å ha brukere, men har ikke tid til å implementere det fullt
+    //Hvis vi faktisk skulle lage et ordentlig produkt hadde vi prioritert dette, men akkurat nå har vi ikke tid
+    //Dette hadde vert klassen som man bruker til å faktisk lage brukere
+    //Hadde nok bare implementert en funksjon for hver brukertype
+
 }
